2f6bbcf2d18d3e0a912c0abb2cce9f4fa2306410
LeandroOliveira12 <leandro.rdeoliveira200@gmail.com>
2022-04-10 20:39:25 -0300

    git status

---
+++ Aula2.py
@@ -0,0 +1,14 @@
+a =  10
+b = 5
+
+soma =  a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+resto = a %b
+
+print (soma)
+print(subtracao)
+print(multiplicacao)
+print(divisao)
+print (resto)
