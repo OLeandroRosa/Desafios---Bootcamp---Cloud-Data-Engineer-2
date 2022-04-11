afab47240eb89e1194e6b8c4c101911497841535
LeandroOliveira12 <leandro.rdeoliveira200@gmail.com>
2022-04-11 13:55:47 -0300

    Aula07_Calculadora2

--- aula07_Calculadora2.py
+++ aula07_Calculadora2.py
@@ -12,9 +12,13 @@ class Calculadora:
     def divisao (self, valor_a, valor_b):
         return valor_a / valor_b
 
-calculadora = Calculadora() # Esta Instanceando a Classe, basicamente é inicializar os parametros/ variaveis que serão utilizados na classe
+calculadora_var = Calculadora() # Esta Instanceando a Classe, basicamente é inicializar os parametros/ variaveis que serão utilizados na classe
 
-print(calculadora.soma(10,2))
-print(calculadora.substracao(5,3))
-print(calculadora.multiplicacao(100,2))
-print(calculadora.divisao(10,5))
+# print('Teste Classes Soma: ',Calculadora.soma(10,2))
+
+print('Calc: ', Calculadora.soma(0,10,2))
+
+print(calculadora_var.soma(10,2))
+print(calculadora_var.substracao(5,3))
+print(calculadora_var.multiplicacao(100,2))
+print(calculadora_var.divisao(10,5))
