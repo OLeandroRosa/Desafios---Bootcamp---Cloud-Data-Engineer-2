aae47817b0666d668484055a486659cdb081d9b5
LeandroOliveira12 <leandro.rdeoliveira200@gmail.com>
2022-04-11 13:28:11 -0300

    Aula07_calculadora01

---
+++ Aula07_Calculadora1.py
@@ -0,0 +1,23 @@
+class Calculadora:
+    def __init__(self, num1, num2):
+        self.valor_a = num1
+        self.valor_b = num2
+
+    def soma (self):
+        return self.valor_a + self.valor_b
+
+    def substracao(self):
+        return self.valor_a - self.valor_b
+
+    def multiplicacao (self):
+        return self.valor_a * self.valor_b
+
+    def divisao (self):
+        return self.valor_a / self.valor_b
+
+calculadora = Calculadora(10, 2) # Esta Instanceando a Classe, basicamente é inicializar os parametros/ variaveis que serão utilizados na classe
+print(calculadora.valor_a)
+print(calculadora.valor_b)
+print(calculadora.soma())
+print(calculadora.substracao())
+
