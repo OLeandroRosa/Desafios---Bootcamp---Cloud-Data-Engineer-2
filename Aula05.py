5d1a3db01598ea7e2be4d201d4d2525891e18f06
LeandroOliveira12 <leandro.rdeoliveira200@gmail.com>
2022-04-11 11:10:25 -0300

    Aula05

---
+++ Aula05.py
@@ -0,0 +1,43 @@
+tupla = (1, 10, 12, 14)
+
+
+print(tupla)
+
+tamanho_tupla =len(tupla)
+print("A Tuple tem", tamanho_tupla, "itens")
+
+
+
+# lista =  [1,3,5,7,9]
+# lista_animal = ["cachorro", "gato", "elefante"]
+# # nova_lista = lista_animal *3
+# print(lista_animal[1])
+#
+# soma = 0
+#
+# for x in lista:
+#     print(x)
+#     soma += x
+# print(sum(lista))
+# print(max(lista))
+#
+# if 'lobo' in lista_animal:
+#     print('existe um lobo na lista')
+# else:
+#     print('Não existe um lobo na lista')
+#     lista_animal.append('Lobo')
+#
+# lista.sort()
+# lista_animal.sort()
+#
+#
+# print(lista)
+# print(lista_animal)
+
+# lista_animal.pop(1)
+# lista_animal.remove('elefante')
+# print(lista_animal)
+#
+# nova_lista = lista_animal *3
+# print(nova_lista)
+
