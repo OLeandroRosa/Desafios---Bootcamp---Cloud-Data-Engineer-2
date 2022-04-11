942b5a215e041d6496ea2881dd2e10708b50e3a0
LeandroOliveira12 <leandro.rdeoliveira200@gmail.com>
2022-04-11 15:37:56 -0300

    Aula08_Exemplo de Importações de módulos

--- Aula08_importacao.py
+++ Aula08_importacao.py
@@ -1,6 +1,17 @@
 from Aula07_televisao import Televisao
+from Aula07_Calculadora1 import Calculadora
+from Aula08_contador_letras import contador_letras, teste
 
-televisao = Televisao()
-print(televisao.ligada)
-televisao.power()
-print(televisao.ligada)
+if __name__ =='__main__' :
+    televisao = Televisao()
+    print(televisao.ligada)
+    televisao.power()
+    print(televisao.ligada)
+
+    calculadora = Calculadora(5,2)
+    print(calculadora.soma())
+
+    lista = ['cachorro', 'gato', 'elefante']
+    total_letras = contador_letras((lista))
+    print('total de letras por palavra de lista: {}'.format(total_letras))
+    print(teste())
