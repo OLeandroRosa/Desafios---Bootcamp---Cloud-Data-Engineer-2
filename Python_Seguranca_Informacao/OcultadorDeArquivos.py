519f95720f96261fe098c5b4789733e21743b204
LeandroOliveira12 <leandro.rdeoliveira200@gmail.com>
2022-04-13 21:48:23 -0300

    Ocultador de Arquivos

--- Python_Seguranca_Informacao/OcultadorDeArquivos.py
+++ Python_Seguranca_Informacao/OcultadorDeArquivos.py
@@ -4,7 +4,7 @@ pasta = input("digite o caminha da pasta a ser ocultadas, exemplo (C:/pasta): ")
 
 atributo_ocultar = 0x02 # determina qual sera o atributo do arquivo oculto
 
-retorno = ctypes.windll.kernel32.SetFileAttributesW('ocultar.txt',atributo_ocultar) #chama na biblioteca ctype a DLL que  permite o que o arquvo seja manipulado e se torne oculto
+retorno = ctypes.windll.kernel32.SetFileAttributesW(pasta,atributo_ocultar) #chama na biblioteca ctype a DLL que  permite o que o arquvo seja manipulado e se torne oculto
 #retorna True ou False
 
 if retorno:
